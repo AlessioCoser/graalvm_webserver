f78542bcf7a04e9bb8bfcd9ec35aa54130ca50bf
Alessio Coser <>
2021-09-28 14:11:01 +0200

    first commit

---
+++ src/test/java/webserver/AppTest.java
@@ -0,0 +1,12 @@
+package webserver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void fixme() {
+        assertTrue(false);
+    }
+}
