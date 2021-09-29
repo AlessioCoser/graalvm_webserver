34272758bafe06e9d415c79047aff8633591cba0
Alessio Coser <>
2021-09-29 13:15:32 +0200

    handle shutdown when press ctrl+c

--- src/main/java/webserver/App.java
+++ src/main/java/webserver/App.java
@@ -2,10 +2,19 @@ package webserver;
 
 
 import spark.Spark;
+import sun.misc.Signal;
 
 public class App {
     public static void main(String[] args) {
         Spark.port(4545);
         Spark.get("/", (req, res) -> "Hello World");
+        handleStopSignal();
+    }
+
+    private static void handleStopSignal() {
+        Signal.handle(new Signal("INT"), signal -> {
+            System.out.println("Interrupted by Ctrl+C");
+            Spark.stop();
+        });
     }
 }
